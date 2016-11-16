2c959aca1349c8a6a8901b817d7527ada5055e32
KriBetko <kribetko@gmail.com>
2016-11-16 10:25:28 +0200

    Push

--- paperCalc.ts
+++ paperCalc.ts
@@ -6,15 +6,18 @@ document.getElementById('paperCalc_reset').addEventListener('click', function ()
     resetInputs();
 });
 
+let inputs: string[] = ["paperCalc_listH", "paperCalc_listW", "paperCalc_objH", "paperCalc_objW"];
+let results: string[] = ["paperCalc_count", "paperCalc_trash", "paperCalc_gap"];
+
 function calc()
 {
+    resetError();
     let listW = parseInt((<HTMLInputElement>document.getElementById("paperCalc_listW")).value);
     let listH = parseInt((<HTMLInputElement>document.getElementById("paperCalc_listH")).value);
     let objW = parseInt((<HTMLInputElement>document.getElementById("paperCalc_objW")).value);
     let objH = parseInt((<HTMLInputElement>document.getElementById("paperCalc_objH")).value);
 
-
-    var checkResult = checkData(listW, listH, objH, objW);
+    let checkResult = checkData(listW, listH, objH, objW);
     if(checkResult != null)
     {
         showError(checkResult);
@@ -22,26 +25,21 @@ function calc()
     else
     {
         let listArea = listH*listW;
-        let objArea = objH*objW;
-        var count = listArea/objArea;
-        var trash = 0;
-
-        if(!isInt(count))
-        {
-            count = Math.floor(count);
-            let totalObjArea = count * objArea - listArea;
-            trash = Math.abs(totalObjArea / (listArea / 100));
-        }
-
-        showResult(count, trash, 0);
+        let count = Math.round(listW/objW) * Math.round(listH/objH);
+        let trash = (listArea - count * (objH*objW)) / (listArea / 100);
+        showResult(count, trash);
     }
 }
 
 function checkData(listW: number = 0, listH: number = 0, objW :number = 0, objH: number = 0):string
 {
-    if(listH <= 0 || listW <= 0 || objH <= 0 || objW <= 0)
+    if(isNaN(listH) || isNaN(listW) || isNaN(objH) || isNaN(objW))
     {
-        return "Все параметри должны быть больше 1";
+        return "Все параметры должны быть определены";
+    }
+    else if(listH <= 0 || listW <= 0 || objH <= 0 || objW <= 0)
+    {
+        return "Все параметры должны быть больше 1";
     }
     else if(objH*objW > listH*listW)
     {
@@ -51,33 +49,35 @@ function checkData(listW: number = 0, listH: number = 0, objW :number = 0, objH:
     return null;
 }
 
-function showResult(count: number, trash: number, gap: number)
+function showResult(count: number, trash: number)
 {
     document.getElementById("paperCalc_count").innerText = count.toString();
     document.getElementById("paperCalc_trash").innerText = trash.toString() + "%";
-    document.getElementById("paperCalc_gap").innerText = gap.toString();
 }
 
 function showError(message: string)
 {
     document.getElementById("paperCalc_error").innerText = message;
+    for (let i = 0; i < inputs.length; i++)
+    {
+        (<HTMLInputElement>document.getElementById(inputs[i])).style.border = "1px solid #ff0000";
+    }
+}
+
+function resetError()
+{
+    document.getElementById("paperCalc_error").innerText = "";
 }
 
 function resetInputs()
 {
-    let inputs: string[] = ["paperCalc_listH", "paperCalc_listW", "paperCalc_objH", "paperCalc_objW"];
-    for (var i = 0; i < inputs.length; i++)
+    for (let i = 0; i < inputs.length; i++)
     {
         (<HTMLInputElement>document.getElementById(inputs[i])).value = "0";
     }
 
-    let results: string[] = ["paperCalc_count", "paperCalc_trash", "paperCalc_gap"];
-    for (i = 0; i < results.length; i++)
+    for (let i = 0; i < results.length; i++)
     {
         document.getElementById(results[i]).innerText = "0";
     }
-}
-
-function isInt(n){
-    return Number(n) === n && n % 1 === 0;
 }
